5e257aaad04038b35bd2958f5708b65336ea25e1
Carlos Estrada <carlos.deg02@hotmail.com>
2018-09-09 00:19:39 -0500

    Added product index.ejs

--- web/routes/product.js
+++ web/routes/product.js
@@ -1,55 +1,56 @@
 var express = require("express"),
-		router = express.Router(),
-		passport = require("passport"),
-		User = require("../models/user"),
-		Product = require("../models/product"),
-		Sales = require("../models/sales");
+  router = express.Router(),
+  passport = require("passport"),
+  User = require("../models/user"),
+  Product = require("../models/product"),
+  Sales = require("../models/sales");
 
 // Show all products of a given business
-router.get("/index",function(req,res) {
-	User.findById(req.user._id,function(err,currUser) {
-		if(err){
-			console.log(err);
-		} else {
-			res.render("/products/index",{products: currUser.products})
-		}
-	})
+router.get("/index", function(req, res) {
+  User.findById(req.user._id, function(err, currUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("/products/index", { products: currUser.products });
+    }
+  });
 });
 
-router.get("/new", isLoggedIn, function(req,res) {
-	res.render("products/new");
+router.get("/new", isLoggedIn, function(req, res) {
+  res.render("products/new");
 });
 
-router.post("/", isLoggedIn , function(req,res) {
-	User.findById(req.user._id, function(err,currUser) {
-		if(err){
-			console.log(err);
-		} else {
-			Product.create({
-			pName: req.body.pName,
-			productType: req.body.productType,
-			price: req.body.precio
-		}, function(err,product) {
-			if(err){
-				console.log(err);
-			} else {
-				currUser.products.push(product);
-				currUser.save();
-				console.log(product);
-				res.redirect("/landing");
-				}
-			})
-		}
-	})
+router.post("/", isLoggedIn, function(req, res) {
+  User.findById(req.user._id, function(err, currUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      Product.create(
+        {
+          pName: req.body.pName,
+          productType: req.body.productType,
+          price: req.body.precio
+        },
+        function(err, product) {
+          if (err) {
+            console.log(err);
+          } else {
+            currUser.products.push(product);
+            currUser.save();
+            console.log(product);
+            res.redirect("/landing");
+          }
+        }
+      );
+    }
+  });
 });
 
-
-function isLoggedIn(req,res,next) {
-	if(req.isAuthenticated()){
-		return next();
-	}
-	res.redirect("/login")
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
 module.exports = router;
-
